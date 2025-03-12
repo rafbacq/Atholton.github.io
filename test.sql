250beec289ff7138ac47b9d88b7f4aa56520b731
notzapped <106430934+notzapped@users.noreply.github.com>
2025-03-11 20:46:21 -0400

    skibibib skiibsrizz

---
+++ test.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE atholtonATCS;
+/*CREATE TABLE teachers(
+    id int NOT NULL,
+    googleID int NOT NULL,
+    firstName varchar(30),
+    curSize int NOT NULL,
+    maxSize int NOT NULL,
+    roomNumber int NOT NULL,
+    className varchar(255),
+    classBio varchar(MAX),
+    PRIMARY KEY (ID)
+)*/
+
+
